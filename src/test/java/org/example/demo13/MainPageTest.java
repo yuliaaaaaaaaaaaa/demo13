2c7986d0dc541a38e55eb95f40923660ee79ff02
yuliaaaaaaaaaaaa <yuliamlfv@gmail.com>
2024-05-31 23:58:55 +0300

    searchTwo added

--- src/test/java/org/example/demo13/MainPageTest.java
+++ src/test/java/org/example/demo13/MainPageTest.java
@@ -1,9 +1,8 @@
 package org.example.demo13;
 
-import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,6 +10,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainPageTest {
     private WebDriver driver;
@@ -42,4 +44,30 @@ public class MainPageTest {
         assertEquals(input, searchPageField.getAttribute("value"));
     }
 
+    @Test
+    public void searchTwo() {
+        String input = "Selenium";
+        WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
+        searchField.sendKeys(input);
+        searchField.submit();
+
+        List<WebElement> results = driver.findElements(By.cssSelector("h2 > a[href]"));
+        clickElement(results, 0);
+
+        driver.getCurrentUrl();
+        assertEquals(results, driver.findElements(By.cssSelector("h2 > a[href]")));
+    }
+
+    public void clickElement(List<WebElement> results, int num) {
+        //
+    }
+//    @Test
+//    public void example() {
+//        List<String> strings = new ArrayList<>();
+//        String a = "First string";
+//        String b = "Second string";
+//        strings.add(a);
+//        strings.add(b);
+//        strings.remove(a);
+//    }
 }
