1599edc8f16f2b450a9a9d3c626f349bad5ef1ae
yuliaaaaaaaaaaaa <yuliamlfv@gmail.com>
2024-06-04 09:43:52 +0300

    WaitList added

--- src/test/java/org/example/demo13/MainPageTest.java
+++ src/test/java/org/example/demo13/MainPageTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
@@ -39,9 +41,12 @@ public class MainPageTest {
         WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
         searchField.sendKeys(input);
         searchField.submit();
-
         WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
         assertEquals(input, searchPageField.getAttribute("value"));
+    }
+
+    public void clickElement(List<WebElement> results, int num) {
+        results.get(num).click();
     }
 
     @Test
@@ -51,23 +56,15 @@ public class MainPageTest {
         searchField.sendKeys(input);
         searchField.submit();
 
-        List<WebElement> results = driver.findElements(By.cssSelector("h2 > a[href]"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.attributeContains(By.cssSelector(":not(ad_url) > cite"), "href", "selenium"),
+                ExpectedConditions.elementToBeClickable(By.cssSelector(":not(ad_url) > cite"))
+        ));
+        List<WebElement> results = driver.findElements(By.cssSelector(":not(ad_url) > cite"));
         clickElement(results, 0);
 
-        driver.getCurrentUrl();
-        assertEquals(results, driver.findElements(By.cssSelector("h2 > a[href]")));
+        assertEquals("https://www.selenium.dev/ ", driver.getCurrentUrl(), "Открылась не та ссылка");
     }
-
-    public void clickElement(List<WebElement> results, int num) {
-        //
-    }
-//    @Test
-//    public void example() {
-//        List<String> strings = new ArrayList<>();
-//        String a = "First string";
-//        String b = "Second string";
-//        strings.add(a);
-//        strings.add(b);
-//        strings.remove(a);
-//    }
 }
+
